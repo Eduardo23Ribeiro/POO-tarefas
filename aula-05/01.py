3471b1b9ee68e8861a441e31e09c5d3fe21d9443
Eduardo <eduardobr728@gmail.com>
2026-04-16 16:47:43 +0000

    COMMIT 2

--- aula-05/01.py
+++ aula-05/01.py
@@ -1,204 +1,227 @@
+# Entidade
 class Triangulo:
     def __init__(self):
-        self._b = 0.0
-        self._h = 0.0
+        self.__b = 0.0
+        self.__h = 0.0
 
     def set_base(self, v):
-        if v >= 0:
-            self._b = v
-        else:
-            raise ValueError()
+        if v >= 0: self.__b = v
+        else: raise ValueError()
 
     def set_altura(self, v):
-        if v >= 0:
-            self._h = v
-        else:
-            raise ValueError()
+        if v >= 0: self.__h = v
+        else: raise ValueError()
 
     def get_base(self):
-        return self._b
-
+        return self.__b
+    
     def get_altura(self):
-        return self._h
-
+        return self.__h
+    
     def calc_area(self):
-        return self._b * self._h / 2
-
+        return self.__b * self.__h / 2
 
 class Circulo:
     def __init__(self):
-        self._raio = 0.0
+        self.__raio = 0.0
 
     def set_raio(self, v):
-        if v >= 0:
-            self._raio = v
-        else:
-            raise ValueError()
+        if v >= 0: self.__raio = v
+        else: raise ValueError()
 
     def get_raio(self):
-        return self._raio
-
+        return self.__raio
+    
     def calc_area(self):
-        return 3.14 * (self._raio ** 2)
-
+        return (self.__raio * 3.14) ** 2
+    
     def calc_circunferencia(self):
-        return 2 * 3.14 * self._raio
-
+        return (self.__raio * 2 * 3.14)
 
 class Viagem:
     def __init__(self):
-        self._distancia = 0.0
-        self._tempo = 0.0
+        self.__distancia = 0.0
+        self.__tempo = 0.0
 
     def set_distancia(self, d):
-        if d >= 0:
-            self._distancia = d
-        else:
-            raise ValueError()
+        if d >= 0: self.__distancia = d
+        else: raise ValueError()
 
     def set_tempo(self, t):
-        if t > 0:
-            self._tempo = t
-        else:
-            raise ValueError()
+        if t >= 0: self.__tempo = t
+        else: raise ValueError()
 
     def get_distancia(self):
-        return self._distancia
-
+        return self.__distancia
+    
     def get_tempo(self):
-        return self._tempo
-
+        return self.__tempo
+    
     def velocidade_media(self):
-        return self._distancia / self._tempo
+        return self.__distancia / self.__tempo
 
-
-class ContaBancaria:
+class Conta_Bancaria:
     def __init__(self):
-        self._titular = ""
-        self._numero = ""
-        self._saldo = 0.0
+        self.__nome = str
+        self.__num_conta = 0.0
+        self.__saldo = 0.0
 
-    def set_titular(self, nome):
-        self._titular = nome
+    def set_nome(self, nome):
+        self.__nome = nome
 
-    def set_numero(self, numero):
-        self._numero = numero
+    def get_nome(self):
+        return self.__nome
+    
+    def set_num_conta(self, v):
+        if v >= 0: self.__num_conta = v
+        else: raise ValueError()
 
-    def get_titular(self):
-        return self._titular
-
-    def get_numero(self):
-        return self._numero
+    def get_num_conta(self):
+        return self.__num_conta
+    
+    def set_saldo(self, v):
+        if v >= 0: self.__saldo = v
+        else: raise ValueError()
 
     def get_saldo(self):
-        return self._saldo
+        return self.__saldo
+    
+    def metodos(self, metodo, valor):
+        if metodo == "d":
+            self.__saldo += valor
+            return valor
 
-    def depositar(self, valor):
-        if valor > 0:
-            self._saldo += valor
+        elif metodo == "s":
+            if valor <= self.__saldo:
+                self.__saldo -= valor
+                return valor
 
-    def sacar(self, valor):
-        if valor > 0 and valor <= self._saldo:
-            self._saldo -= valor
-
-    def dados(self):
-        return self._titular, self._numero, self._saldo
-
+        else: raise ValueError()
 
 class Ingresso:
     def __init__(self):
-        self._dia = ""
-        self._hora = 0
-
-    def set_dia(self, dia):
-        self._dia = dia.lower()
-
-    def set_hora(self, hora):
-        self._hora = hora
-
+        self.__dia = str
+        self.__horario = 0.0
+    
+    def set_dia(self, d):
+        if d == 'segunda' or d == 'terça' or d == 'quarta' or d == 'quinta' or d == 'sexta' or d == 'sabado' or d == 'domingo':
+            self.__dia = d
+        else: raise ValueError()
+    
     def get_dia(self):
-        return self._dia
+        return self.__dia
 
-    def get_hora(self):
-        return self._hora
+    def set_horario(self, h):
+        if 0 <= h <= 23:
+            self.__horario = h
+        else: raise ValueError()
+    
+    def get_horario(self):
+        return self.__horario
+    
+    def calc_ingresso(self, idade):
 
-    def valor_base(self):
-        if self._dia in ["segunda", "terca", "quinta"]:
-            return 16
-        elif self._dia == "quarta":
-            return 8
-        elif self._dia in ["sexta", "sabado", "domingo"]:
-            return 20
+        if 1 <= idade <= 18: 
+            if self.__dia == 'segunda' or self.__dia == 'terça' or self.__dia == 'quinta':
+                return f'R$ 8,00'
+            
+            elif self.__dia == 'quarta':
+                return f'R$ 8,00'
+            
+            elif self.__dia == 'sexta' or self.__dia == 'sabado' or self.__dia == 'domingo':
+                if self.__horario >= 17:
+                    return f'R$ 15,00'
+                else:
+                    return f'R$ 10,00'
+            
+        elif 19 <= idade <= 200:
+            if self.__dia == 'segunda' or self.__dia == 'terça' or self.__dia == 'quinta':
+                return f'R$ 16,00'
+            
+            elif self.__dia == 'quarta':
+                return f'R$ 8,00'
+            
+            elif self.__dia == 'sexta' or self.__dia == 'sabado' or self.__dia == 'domingo':
+                if self.__horario >= 17:
+                    return f'$ 30,00'
+                else:
+                    return f'R$ 20,00'
 
-    def valor_ingresso(self):
-        base = self.valor_base()
-        if self._dia == "quarta":
-            return base
-        if 17 <= self._hora <= 23:
-            base *= 1.5
-        return base
-
-    def meia_entrada(self):
-        return self.valor_ingresso() / 2
+        else: raise ValueError()
 
 
+
+# Interface com usuário (User Interface) - prints, inputs
 class UI:
     @staticmethod
     def main():
         op = 0
         while op != 9:
             op = UI.menu()
-            if op == 1:
-                UI.triangulo()
-            elif op == 2:
-                UI.circulo()
-            elif op == 3:
-                UI.viagem()
-            elif op == 4:
-                UI.conta()
-            elif op == 5:
-                UI.ingresso()
+            if op == 1: UI.triangulo()
+            if op == 2: UI.circulo()
+            if op == 3: UI.viagem()
+            if op == 4: UI.conta_bancaria()
+            if op == 5: UI.ingresso()
 
     @staticmethod
     def menu():
-        print("1-Triângulo 2-Círculo 3-Viagem 4-Conta Bancária 5-Ingresso 9-Fim")
-        return int(input("Informe uma opção: "))
+        print("1-Triângulo, 2-Círculo, 3-Viagem, 4-Conta Bancária, 5-Ingresso, 9-Fim")
+        op = int(input("Informe uma opção: "))
+        return op    
 
     @staticmethod
     def triangulo():
+        print("Cálculo da área do triângulo")
         x = Triangulo()
-        x.set_base(float(input("Base: ")))
-        x.set_altura(float(input("Altura: ")))
-        print(x.calc_area())
+        x.set_base(float(input("Informe o valor da base: ")))     # método de instância
+        x.set_altura(float(input("Informe o valor da altura: ")))
+        area = x.calc_area()
+        print(f"Um triângulo com base {x.get_base()} e altura {x.get_altura()} tem área = {area}")
+        print("="*40)
 
     @staticmethod
     def circulo():
+        print("Cálculo da área e circunferencia do Circulo")
         x = Circulo()
-        x.set_raio(float(input("Raio: ")))
-        print(x.calc_area(), x.calc_circunferencia())
+        x.set_raio(float(input("Informe o valor da raio: ")))     # método de instância
+        area = x.calc_area()
+        circunferencia = x.calc_circunferencia()
+        print(f"Um Circulo com raio {x.get_raio()}, tem área = {area}, e circunferencia = {circunferencia}")
+        print("="*40)
 
     @staticmethod
     def viagem():
-        v = Viagem()
-        v.set_distancia(float(input("Distância: ")))
-        v.set_tempo(float(input("Tempo (h): ")))
-        print(v.velocidade_media())
+        print("Cálculo da Velocidade Media")
+        x = Viagem()
+        x.set_distancia(float(input("Informe o valor da distancia: ")))     # método de instância
+        x.set_tempo(float(input("Informe o valor do tempo: ")))     # método de instância
+        vel_med = x.velocidade_media()
+        print(f"Uma Velocidade Media com distancia = {x.get_distancia()}, e tempo = {x.get_tempo()}. Tem Velocidade Media = {vel_med}km/h")
+        print("="*40)
 
     @staticmethod
-    def conta():
-        c = ContaBancaria()
-        c.set_titular(input("Nome: "))
-        c.set_numero(input("Número: "))
-        c.depositar(float(input("Depósito: ")))
-        c.sacar(float(input("Saque: ")))
-        print(c.dados())
+    def conta_bancaria():
+        print("Conta Bancária")
+        x = Conta_Bancaria()
+        x.set_nome((input("Informe o seu Nome: ")))
+        x.set_num_conta(float(input("Informe o Número da Conta: ")))     # método de instância
+        x.set_saldo(float(input("Informe o seu Saldo: ")))
+        operacao = input('Digite "d" para DEPOSITO ou "s" para SAQUE: ').lower().strip()
+        valor = float(input("Digite o valor: "))     # método de instância
+        metodos = x.metodos(operacao, valor)
+        print(f"Conta Bancaria de {x.get_nome()}, tem numero de conta: {x.get_num_conta()}, Saldo Antigo: {x.get_saldo():.2f}\n Novo Valor Bancario = {metodos:.2f}")
+        print("="*40)
 
     @staticmethod
     def ingresso():
-        i = Ingresso()
-        i.set_dia(input("Dia: "))
-        i.set_hora(int(input("Hora: ")))
-        print(i.valor_ingresso(), i.meia_entrada())
-
+        print("Ingressos")
+        x = Ingresso()
+        x.set_dia((input("Informe o dia: ")))     # método de instância
+        x.set_horario(float(input("Informe o horario: ")))     # método de instância
+        idade = float(input("Digite a sua Idade: "))
+        calculo_ingresso = x.calc_ingresso(idade)
+        print(f"Seu Ingresso que é do dia: {x.get_dia()}, e tem horario às: {x.get_horario()}. Sai pelo valor de: {calculo_ingresso}")
+        print("="*40)    
 
 UI.main()
