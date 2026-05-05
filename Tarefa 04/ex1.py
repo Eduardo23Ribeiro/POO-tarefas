ff729693a73cbe3fbd99dd0a2d0e37921e734ede
Eduardo23Ribeiro <eduardobr728@gmail.com>
2026-05-05 18:57:12 +0000

    Arrumar2

--- Tarefa 04/ex1.py
+++ Tarefa 04/ex1.py
@@ -25,7 +25,7 @@ class Time:
     def get_estadofederacao(self):
         return self.estadofederacao
     def __str__(self):
-        return f"ID = {self.id} - Nome = {self.nome} - Estado federativo = {self.estadofederacao}"
+        return f"ID = {self.id} - Nome = {self.nome}"
 class Jogador:
     def __init__(self,id,nome,camisa,idtime):
         self.set_id(id)
@@ -62,3 +62,97 @@ class Jogador:
         return self.idioma
     def __str__(self):
         return f"ID = {self.id} - Nome = {self.nome}"
+class UI:
+    time = []
+    jogadores = []
+
+    @staticmethod
+    def main():
+        op = 0
+        while op != 6:
+            op = UI.menu()
+            if op == 1:
+                UI.inserir_time()
+            elif op == 2:
+                UI.listar_time()
+            elif op == 3:
+                UI.atualizar_time()
+            elif op == 4:
+                UI.excluir_times()
+            elif op == 5:
+                UI.inserir_jogador()
+            elif op == 6:
+                UI.listar_jogador()
+            elif op == 7:
+                UI.atualizar_jogador()
+            elif op == 8:
+                UI.excluir_jogador()
+            elif op == 9:
+                UI.sair()
+                print("Fim do programa")
+            
+          
+
+    @staticmethod
+    def menu():
+        print("1-Inserir Time 2-Listar Times 3-Atualizar Time 4-Excluir Times 5-Inserir Jogador 6-Listar Jogadores 7-Atualizar Jogador 8-Excluir Jogador 9-Sair")
+        return int(input("Escolha uma opção: "))    
+    @classmethod
+    def inserir_time(cls):
+        id = int(input("Digite o ID do time: "))
+        nome = int(input("Digite o nome do time: "))
+        estadofederacao = int(input("Digite o estado da fedação do time "))
+        y = Time(id,nome,estadofederacao)
+        cls.time.append(y)
+        print("Time inserido com sucesso!")
+    @classmethod
+    def listar_time(cls):
+        for x in cls.time:
+            print(x)
+    @classmethod
+    def atualizar_time(cls): 
+        id = int(input("Digite o ID do time a ser atualizado: "))
+        for x in cls.time:
+            if x.get_id() == id:
+                nome = int(input("Digite o novo nome do time: "))
+                estadofederacao = int(input("Digite o novo estado da federação do time: "))
+                x.set_nome(nome)
+                x.set_estadofederacao(estadofederacao)
+                print("Time atualizado com sucesso!")
+                return
+        print("Time não encontrado.")
+    @classmethod
+    def excluir_times(cls):
+        id = int(input("Digite o ID do time a ser excluido: "))
+        for x in cls.time:
+            if x.get_id() == id:
+                cls.time.remove(x)
+                print("Time excluido com sucesso!")
+                return
+    def inserir_jogador(cls):
+        id = int(input("Digite o ID do jogador: "))
+        nome = int(input("Digite o nome do jogador: "))
+        idtime = int(input("Digite o ID do time do jogador: "))
+        camisa = int(input("Digite o número da camisa do jogador:"))
+        y = Time(id,idtime,camisa)
+        cls.time.append(y)
+        print("Time inserido com sucesso!")
+    def listar_jogador(cls):
+        for x in cls.jogador:
+            print(x)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UI.main()
