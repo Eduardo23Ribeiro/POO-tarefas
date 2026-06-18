2675b8a7ebe7504c27895e02fa941d978f2c86ff
Eduardo23Ribeiro <eduardobr728@gmail.com>
2026-06-18 17:24:10 +0000

    neuron activated

--- Aula_08/ex3.py
+++ Aula_08/ex3.py
@@ -1,3 +1,4 @@
+import json
 class Cliente:
     def __init__(self, nome, idade):
         self.nome = nome
@@ -9,15 +10,30 @@ class Cliente:
     @staticmethod
     def from_json(dic):
         return Cliente(dic["id"], dic["nome"])
-    
-a = Cliente(1,"Jon kook")
-b = Cliente(2,"Jimin kim jon um")
-c = Cliente.from_json({"id": 3, "nome": "Kim taehyung"})
+def salvar():    
+    a = Cliente(1,"Jon kook")
+    b = Cliente(2,"Jimin kim jon um")
+    c = Cliente.from_json({"id": 3, "nome": "Kim taehyung"})
 
-print(a)
-print(b)
-print(c)
-print(a.__dict__)
-print(b.__dict__)
-print(vars(a))
-print(vars(b))
+    lista = [a, b, c]
+
+    arquivo = open("clientes.json", mode="w")
+    json.dump(lista,arquivo,default = Cliente.to_json, indent = 2)
+    arquivo.close()
+
+
+    print(a)
+    print(b)
+    print(c)
+    print(a.__dict__)
+    print(b.__dict__)
+    print(c.__dict__)
+def abrir():
+    arquivo = open("clients.json", mode="r")
+    list_dic = json.load(arquivo)
+    for dic in list_dic:
+        x = Cliente.from_json(dic)
+        print(x)
+#salvar()
+abrir()
+
